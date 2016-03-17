a8822eb5ee0c55a8ea689e1b4e4b9bc7d9f08857
Leon Paternoster <leon.paternoster@zoho.com>
2016-03-17 20:58:29 +0000

    Hamburger

---
+++ js/app.js
@@ -0,0 +1,44 @@
+// Call Google font-size
+
+WebFontConfig = {
+google: { families: [ 'Source+Sans+Pro:400,400italic,600,700:latin' ] }
+};
+(function() {
+var wf = document.createElement('script');
+wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+wf.type = 'text/javascript';
+wf.async = 'true';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(wf, s);
+})();
+
+// Toggles hide class on .toggle when .toggle-button clicked
+
+document.addEventListener('DOMContentLoaded', function() {
+	[].forEach.call(document.querySelectorAll('.toggle-button'), function(el) {
+		el.addEventListener('click', function(e) {
+			if (e.target.nodeName.toLowerCase() == 'a') {
+				e.preventDefault();
+			}
+			[].forEach.call(document.querySelectorAll('.toggle'), function(el) {
+				el.classList.toggle('hide');
+			});
+		});
+	});
+});
+
+
+// Toggles open class on .toggle-button when .toggle-button clicked
+
+document.addEventListener('DOMContentLoaded', function() {
+	[].forEach.call(document.querySelectorAll('.toggle-button'), function(el) {
+		el.addEventListener('click', function(e) {
+			if (e.target.nodeName.toLowerCase() == 'a') {
+				e.preventDefault();
+			}
+			[].forEach.call(document.querySelectorAll('.toggle-button'), function(el) {
+				el.classList.toggle('open');
+			});
+		});
+	});
+});
