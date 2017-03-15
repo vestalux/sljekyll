2c6a640135dc1ff2dca2f40821ef76f8e89d4239
Leon Paternoster <leon.paternoster@suffolklibraries.co.uk>
2017-03-15 14:49:49 +0000

    Resized and using default sans

--- _site/js/app.js
+++ _site/js/app.js
@@ -1,17 +1,3 @@
-// Call Google font-size
-
-WebFontConfig = {
-google: { families: [ 'Source+Sans+Pro:300,400,400italic,500,600,700,700italic:latin' ] }
-};
-(function() {
-var wf = document.createElement('script');
-wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-wf.type = 'text/javascript';
-wf.async = 'true';
-var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(wf, s);
-})();
-
 // Toggles db class on .custom-toggle when .custom-toggle-button clicked
 
 document.addEventListener('DOMContentLoaded', function() {
